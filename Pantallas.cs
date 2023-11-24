ccbea81574d142a69d6ac60c4f975e7a92eb6535
User <User@DESKTOP-CRJD420>
2023-11-24 13:14:58 -0500

    Agregar archivos de proyecto.

---
+++ Pantallas.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab13
+{
+    public static class Pantallas
+    {
+        public static string[] respuestas = new string[100];
+        public static int cantidadPersonas = 0;
+        public static void encuesta()
+        {
+            Console.Clear();
+            Console.Write("===================================\n" +
+                      "Nivel de Satisfacción\n" +
+                      "===================================\n" +
+                      "¿Qué tan satisfecho está con la atención de nuestra tienda?\n" +
+                      "1: Nada satisfecho\n" +
+                      "2: No muy satisfecho\n" +
+                      "3: Tolerable\n" +
+                      "4: Satisfecho\n" +
+                      "5: Muy satisfecho\n" +
+                      "===================================\n" +
+                      "Ingrese una opción: \n");
+            string respuesta = Console.ReadLine();
+            if (EsRespuestaValida(respuesta))
+            {
+                respuestas[cantidadPersonas] = respuesta;
+                cantidadPersonas++;
+                Console.Clear();
+                Console.Write("===================================\n" +
+                      "Nivel de Satisfacción\n" +
+                      "===================================\n" +
+                      "¡Gracias por participar!\n" +
+                      "===================================\n" +
+                      "Presione cualquier tecla para regresar al menú ...\n");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.Clear();
+                Console.Write("Respuesta no válida. Por favor, ingrese un número del 1 al 5." +
+                    "\nPresione cualquier tecla para continuar.");
+                Console.ReadKey();
+                encuesta();
+            }
+            Console.Clear();
+        }
+        public static void DatosRegistrados()
+        {
+            Console.Clear();
+            Console.WriteLine("===================================\n" +
+                "Ver datos registrados\n" +
+                "===================================");
+            datosRegistrados();
+            Console.WriteLine("\n===================================");
+            Console.WriteLine($"\n{ContarOcurrencias("1"):D2} personas: Nada satisfecho\n" +
+                  $"{ContarOcurrencias("2"):D2} personas: No muy satisfecho\n" +
+                  $"{ContarOcurrencias("3"):D2} personas: Tolerable\n" +
+                  $"{ContarOcurrencias("4"):D2} personas: Satisfecho\n" +
+                  $"{ContarOcurrencias("5"):D2} personas: Muy satisfecho\n" +
+                  "Presione cualquier tecla para regresar al menú");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public static void eliminarDato()
+        {
+            Console.Clear();
+            Console.WriteLine("===================================\n" +
+                  "Eliminar un dato\n" +
+                  "===================================");
+            datosRegistrados();
+            Console.WriteLine("\n===================================" +
+                  "\nIngrese la posición a eliminar: ");
+            if (int.TryParse(Console.ReadLine(), out int indice) && indice >= 0 && indice < cantidadPersonas)
+            {
+                for (int i = indice; i < cantidadPersonas - 1; i++)
+                {
+                    respuestas[i] = respuestas[i + 1];
+                }
+                cantidadPersonas--;
+                Console.WriteLine("===================================\n" +
+                      "Respuesta eliminada\n" +
+                      "===================================\n");
+            }
+            else
+            {
+                Console.WriteLine("Índice no válido. Inténtelo de nuevo.");
+            }
+            datosRegistrados();
+            Console.WriteLine("\n===================================");
+            Console.Write("\nPresione cualquier tecla para volver al menú\n");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public static void ordenar()
+        {
+            Console.Clear();
+            Console.WriteLine("===================================\n" +
+                  "Ordenar datos\n" +
+                  "===================================");
+            datosRegistrados();
+            Console.WriteLine("\nPresione cualquier tecla para ordenar");
+            Console.WriteLine("============================================");
+            Array.Sort(respuestas, 0, cantidadPersonas);
+            Console.Write("");
+            Console.ReadKey();
+            datosRegistrados();
+            Console.WriteLine("\nPresione cualquier tecla para volver al menú");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public static bool EsRespuestaValida(string respuesta)
+        {
+            return respuesta == "1" || respuesta == "2" || respuesta == "3" || respuesta == "4" || respuesta == "5";
+        }
+        public static int ContarOcurrencias(string opcion)
+        {
+            int contador = 0;
+            for (int i = 0; i < cantidadPersonas; i++)
+            {
+                if (respuestas[i] == opcion)
+                {
+                    contador++;
+                }
+            }
+            return contador;
+        }
+        
+        public static void datosRegistrados()
+        {
+            for (int i = 0; i < cantidadPersonas; i++)
+            {
+                Console.Write($"{i:D3}:[{respuestas[i]}]");
+
+                if ((i + 1) % 5 == 0)
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write("  ");
+                }
+            }
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+            do
+            {
+                Console.Write("================================\n" +
+                  "Encuestas de Calidad\n" +
+                  "================================\n" +
+                  "1: Realizar Encuesta\n" +
+                  "2: Ver datos registrados\n" +
+                  "3: Eliminar un dato\n" +
+                  "4: Ordenar datos de menor a mayor\n" +
+                  "5: Salir\n" +
+                  "================================\n" +
+                  "Ingrese una opción: \n");
+                if (int.TryParse(Console.ReadLine(), out opcion))
+                {
+                    switch (opcion)
+                    {
+                        case 1: Pantallas.encuesta(); break;
+                        case 2: Pantallas.DatosRegistrados(); break;
+                        case 3: Pantallas.eliminarDato(); break;
+                        case 4: Pantallas.ordenar(); break;
+                        case 5: Console.Clear(); break;
+                        default:
+                            Console.Clear(); Console.Write("Opción no válida. Inténtelo de nuevo." +
+                            "\nPrecione cualquier tecla para continuar"); Console.ReadKey(); break;
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.Write("Por favor, ingrese un número válido." +
+                          "\nPresione cualquier tecla para continuar");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+            } while (opcion != 5);
+        }
+    }
+}
